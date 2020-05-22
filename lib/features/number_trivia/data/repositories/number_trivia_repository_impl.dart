d600eae47dc1739ee0427288add03f4b552bed46
kiranreddykasa <kiranreddy2004@gmail.com>
2020-05-22 18:58:10 +0530

    Network info implementation

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,6 +1,6 @@
 import 'package:clean_architecture_tdd/core/error/exceptions.dart';
 import 'package:clean_architecture_tdd/core/error/failures.dart';
-import 'package:clean_architecture_tdd/core/platform/network_info.dart';
+import 'package:clean_architecture_tdd/core/network/network_info.dart';
 import 'package:clean_architecture_tdd/features/number_trivia/data/datasources/number_trivia_local_datasource.dart';
 import 'package:clean_architecture_tdd/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart';
 import 'package:clean_architecture_tdd/features/number_trivia/domain/entities/number_trivia.dart';
